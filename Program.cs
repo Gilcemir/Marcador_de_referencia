345c9bf8f2b42d9b7fe78c7e59ae59de2b52cf69
Gilcemir <gangelofilho@gmail.com>
2022-01-21 15:45:25 -0300

    Lista com referências tagueadas em Autores e Date pronto - ainda sem Output file

--- Program.cs
+++ Program.cs
@@ -1,36 +1,73 @@
 ﻿using System;
-using Marcador_de_referencia;
 using System.IO;
+using System.Text.RegularExpressions;
+using Marcador_de_referencia;
 
 namespace Program
 {
-
     class Tela_Principal
     {
+        //Essa função divide a referência em 3 partes: Autores, ano e o resto. Estou fazendo pouco a pouco, então por enquanto se
+
+        // o programa só marcar os autores e data automático, já vai me economizar bastante tempo...
+        private static void SplitRef(
+            string referencia,
+            ref string autores,
+            ref string date,
+            ref string body
+        )
+        {
+            string pattern = @"(.*)\((\d{4})\)(.*)";
+            Regex r = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            Match m = r.Match(referencia);
+
+            autores = m.Groups[1].ToString();
+            date = m.Groups[2].ToString();
+            body = m.Groups[3].ToString();
+        }
+
         static void Main(string[] args)
         {
-           string path = @"C:\workspace\Marcador_de_referencia\";
+            string path = @"C:\workspace\Marcador_de_referencia\";
             Console.WriteLine("Entre o nome do arquivo: ");
-            path+=Console.ReadLine();
+            path += Console.ReadLine();
 
-            List<string> referencias = new List<string>{};         
-            
+            List<string> referencias = new List<string> { };
+            List<string> referenciasTag = new List<string> { };
             using (StreamReader sr = File.OpenText(path))
             {
-            string s;
+                string s;
                 while ((s = sr.ReadLine()) != null)
                 {
-                referencias.Add(s);
+                    if (s.Trim().Length > 0) referencias.Add(s);
                 }
             }
-
-            foreach(string referencia in referencias)
+          
+            
+            int i = 1;
+            foreach (string referencia in referencias)
             {
-                Console.WriteLine(referencia);
+                string autores="";
+                string date="";
+                string body="";
+                SplitRef(referencia, ref autores, ref date, ref body);
+                string temporaria =
+                    RefMkp
+                        .TagRef((
+                        RefMkp.TagAuthors(autores) + RefMkp.TagDate(date)
+                        ) +
+                        body,
+                        i,
+                        "journal");
+                referenciasTag.Add (temporaria);
+                i++;
             }
 
+            foreach (string referencia in referenciasTag)
+            {
+                Console.WriteLine (referencia);
+            }
         }
     }
-
-
 }
--- RefMkp.cs
+++ RefMkp.cs
@@ -1,68 +1,94 @@
 using System;
 using System.Linq;
+
 namespace Marcador_de_referencia
 {
     public class RefMkp
     {
         //marca tag simples, string e tag como parâmetro
+        /*
+            RefMkp.TagSimples("aaaaa", "source")
+
+            Retorna [source]aaaaa[/source]
+
+        */
         public static string TagSimples(string str, string tag)
         {
-            return "["+tag+"]"
-            +str
-            +"[/"+tag+"]";
+            return "[" + tag + "]" + str + "[/" + tag + "]";
         }
 
         //marca tag ref, recebe como parâmetro a referencia, o ID e o tipo
+        /*
+            RefMkp.TagRef("aaaaa", 1, "book")
+
+            Retorna [ref id="r1" reftype="book"]aaaaa[/ref]
+
+        */
         public static string TagRef(string str, int id, string tipo)
         {
-            return "[ref id=\"r"+ id +"\" retype=\""+tipo+"\"]"
-            +str
-            +"[/ref]";
+            return "[ref id=\"r" +
+            id +
+            "\" retype=\"" +
+            tipo +
+            "\"]" +
+            str +
+            "[/ref]";
         }
 
         //marca tag Date, recebe como parâmetro o ano
+        /*
+            RefMkp.TagDate(2011) ou RefMkp.TagDate("2011")
+
+            Retorna [date dateiso="20110000" specyear="2011"]2011[/date]
+
+        */
         public static string TagDate(object year)
         {
-            return "[date dateiso=\""+year+"0000\" specyear=\""+year+"\"]"
-            +year
-            +"[/date]";
+            return "[date dateiso=\"" +
+            year +
+            "0000\" specyear=\"" +
+            year +
+            "\"]" +
+            year +
+            "[/date]";
         }
 
         //recebe a string com autores e retorna os autores taggeados
         public static string TagAuthors(string authors)
         {
-        string str ="[authors role=\"nd\"][pauthor]";
-        string[] delimitadores = new string[] {", ", " and ", "&"};
-       
-        string[] autores = authors
-                                .Split(delimitadores, StringSplitOptions.RemoveEmptyEntries)
-                                .ToList()
-                                .Select(p=>p.Trim())
-                                .ToArray();
+            string str = "[authors role=\"nd\"][pauthor]";
+            string[] delimitadores = new string[] { ", ", " and ", "&" };
 
-        foreach(string autor in autores)
+            string[] autores =
+                authors
+                    .Split(delimitadores, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList()
+                    .Select(p => p.Trim())
+                    .ToArray();
+
+            foreach (string autor in autores)
+            {
+                string aut = "";
+                string[] ASplitted = autor.Split(' ');
+
+                if (ASplitted.Length > 2)
                 {
-                    string aut= "";
-                    string[] ASplitted = autor.Split(' ');
+                    for (int i = 0; i < ASplitted.Length - 1; i++)
+                    aut += ASplitted[i] + " ";
 
-                    if(ASplitted.Length>2)
-                    {
-                        for(int i=0; i<ASplitted.Length-1;i++) aut+=ASplitted[i]+" ";
-                        
-                        aut = aut.Trim();
-                    }else 
-                        aut = ASplitted[0];
-
-                    string temp = TagSimples(aut, "surname") + TagSimples( ASplitted[ASplitted.Length-1], "fname");
-                    str+=temp;
+                    aut = aut.Trim();
                 }
-                
+                else
+                    aut = ASplitted[0];
 
-        str+="[/pauthor][/authors]";
-        return str;
+                string temp =
+                    TagSimples(aut, "surname") +
+                    TagSimples(ASplitted[ASplitted.Length - 1], "fname");
+                str += temp;
+            }
+
+            str += "[/pauthor][/authors]";
+            return str;
         }
-
-
     }
-
 }
